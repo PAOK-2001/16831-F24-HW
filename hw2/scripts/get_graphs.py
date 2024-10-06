b0a2a4576d2ff8b7ba91d822584728df8b4052cf
Pablo Ortega <2001.paok@gmail.com>
2024-10-05 22:06:18 -0400

    Clean up for submission

--- hw2/scripts/get_graphs.py
+++ hw2/scripts/get_graphs.py
@@ -1,44 +1,78 @@
 import os
 import re
-from datetime import datetime
 from tensorboard.backend.event_processing import event_accumulator
 import matplotlib.pyplot as plt
 
 out_dir = 'results/'
 
+EXP_DICT = {
+    "Small Batch Experiments": [
+        "q1_sb_no_rtg_dsa_CartPole-v0_04-10-2024_02-35-16",
+        "q1_sb_rtg_dsa_CartPole-v0_04-10-2024_02-36-29",
+        "q1_sb_rtg_na_CartPole-v0_04-10-2024_02-37-42"
+    ],
+
+    "Large Batch Experiments":[      
+        "q1_lb_no_rtg_dsa_CartPole-v0_04-10-2024_03-09-59",
+        "q1_lb_rtg_dsa_CartPole-v0_04-10-2024_03-15-23",
+        "q1_lb_rtg_na_CartPole-v0_04-10-2024_03-20-00"
+    ],
+
+    "Inverted Pendulum": ["q2_b100_r0.02_rtg_InvertedPendulum-v4_04-10-2024_02-52-47"],
+    "Lunar Lander": ["q3_b10000_r0.005_LunarLanderContinuous-v2_04-10-2024_23-23-40"],
+
+    "Half Cheetah Experiments": [
+        "q4_search_b10000_lr0.02_HalfCheetah-v4_04-10-2024_23-54-05",
+        "q4_search_b10000_lr0.02_nnbaseline_HalfCheetah-v4_05-10-2024_00-18-03",
+        "q4_search_b10000_lr0.02_rtg_HalfCheetah-v4_05-10-2024_00-06-54",
+        "q4_search_b10000_lr0.02_rtg_nnbaseline_HalfCheetah-v4_05-10-2024_00-30-36"
+    ],
+
+    "Half Cheetah Hyperparameter Sweep":[
+        "q4_b10000_lr0.01-rtg_nn_baseline_HalfCheetah-v4_05-10-2024_03-53-42",
+        "q4_b10000_lr0.02-rtg_nn_baseline_HalfCheetah-v4_05-10-2024_04-40-57",
+        "q4_b10000_lr0.005-rtg_nn_baseline_HalfCheetah-v4_05-10-2024_03-06-22",
+        "q4_b30000_lr0.01-rtg_nn_baseline_HalfCheetah-v4_05-10-2024_08-55-09",
+        "q4_b30000_lr0.02-rtg_nn_baseline_HalfCheetah-v4_05-10-2024_11-13-15",
+        "q4_b30000_lr0.005-rtg_nn_baseline_HalfCheetah-v4_05-10-2024_06-37-47",
+        "q4_b50000_lr0.01-rtg_nn_baseline_HalfCheetah-v4_05-10-2024_17-46-21",
+        "q4_b50000_lr0.02-rtg_nn_baseline_HalfCheetah-v4_05-10-2024_18-40-50",
+        "q4_b50000_lr0.005-rtg_nn_baseline_HalfCheetah-v4_05-10-2024_14-40-28"
+    ],
+
+    "Optimal Hyperparameters": [
+        "q4_optimal_b30000_lr0.02_HalfCheetah-v4_05-10-2024_20-09-29",
+        "q4_optimal_b30000_lr0.02_rtg_HalfCheetah-v4_05-10-2024_20-47-37",
+        "q4_b30000_lr0.02-rtg_nn_baseline_HalfCheetah-v4_05-10-2024_11-13-15",
+        "q4_optimal_nnbaseline_HalfCheetah-v4_05-10-2024_21-20-52"
+    ],
+
+    "Generalized Advantage Estimation": [
+        "q5_b2000_r0.001_baseline_Hopper-v4_04-10-2024_22-39-34",
+        "q5_b2000_r0.001_lambda0_Hopper-v4_04-10-2024_22-48-09",
+        "q5_b2000_r0.001_lambda0.95_Hopper-v4_04-10-2024_22-56-19",
+        "q5_b2000_r0.001_lambda0.99_Hopper-v4_04-10-2024_23-05-27",
+        "q5_b2000_r0.001_lambda1_Hopper-v4_04-10-2024_23-14-16",
+    ],
+}
+
 def parse_string(s):
-    # Regular expression to match the parameters
     pattern = r"b(\d+)_lr([0-9.]+)"
     learning_params = re.search(pattern, s)
-    # Init tags
     batch_size = ""
     lr = ""
     nn_baseline = ""
     rtg = ""
-
     if learning_params:
         batch_size = learning_params.group(1)
         lr = learning_params.group(2)
 
-    if "--nn_bassline" in s:
+    if "_nn_baseline" in s or "nnbaseline" in s:
         nn_baseline = "_nn_baseline"
     if "rtg" in s:
         rtg = "_rtg"
-
-    if nn_baseline:
-        nn_baseline = nn_baseline.group(1)
     return f"b{batch_size}_lr{lr}{nn_baseline}{rtg}"
 
-questions = {
-    # "Small Batch Experiments": "q1_sb",
-    # "Large Batch Experiments": "q1_lb",
-    # "Inverted Pendulum": "q2_b100_r0.02_rtg_InvertedPendulum-v4_04-10-2024_02-52-47",
-    # "Lunar Lander": "q3_b10000_r0.005_LunarLanderContinuous-v2_04-10-2024_23-23-40",
-    # "Half Cheetah Experiments": "q4_search",
-    # "Half Cheetah Hyperparameter Sweep": "q7_",
-    "Optimal Hyperparameters": "q7_optimal",
-    # "Generalized Advantage Estimation": "q5_",
-}
 
 def extract_scalar_events(logdir, tag):
     scalar_events = []
@@ -55,7 +89,7 @@ def plot_graph(logdirs, tag, output_file, title):
         scalar_events = extract_scalar_events(logdir, tag)
         if scalar_events:
             steps, values = zip(*scalar_events)
-            if "q7" in experiment_tag:
+            if "q4" in experiment_tag:
                 experiment_tag = parse_string(experiment_tag)
             plt.plot(steps, values, label=experiment_tag, linewidth=2.5)  # Set line width to 2.5
     
@@ -68,11 +102,9 @@ def plot_graph(logdirs, tag, output_file, title):
 
 if __name__ == "__main__":
     logdir = 'data/'
-
-    for exp, question in questions.items():
-        # Find all dirs with prefix question
-        logdirs = [os.path.join(logdir, d) for d in os.listdir(logdir) if os.path.isdir(os.path.join(logdir, d)) and question in d]
-        
+    os.makedirs(out_dir, exist_ok=True)
+    for exp, logdirs in EXP_DICT.items():
+        logdirs = [f"{logdir}{log}" for log in logdirs]        
         tag = 'Eval_AverageReturn'
         output_file = f'{out_dir}/{exp}.png'
         title = exp
--- hw2/scripts/q7_cheetah_hparam.sh
+++ hw2/scripts/q7_cheetah_hparam.sh
@@ -4,35 +4,19 @@ set -e
 
 echo "Running Q7 Cheetah Hyperparameter Search"
 
-# Consider B sizes 10000, 30000, 50000
-# Consider learning rates 0.005, 0.01, 0.02
-# Use try different rtg  and nn_baseline settings
-# for B in 10000 30000 50000
-# do
-#     for LR in 0.005 0.01 0.02
-#     do
-#         for RTG in "" "-rtg"
-#         do
-#             for NN_BASELINE in "" "--nn_baseline"
-#             do
-#                 echo "Running with B=$B LR=$LR RTG=$RTG NN_BASELINE=$NN_BASELINE"
-#                 python rob831/scripts/run_hw2.py --env_name HalfCheetah-v4 --ep_len 150 --discount 0.95 -n 100 -l 2 -s 32 -b $B -lr $LR $RTG $NN_BASELINE --exp_name q7_b$B\_lr$LR$RTG$NN_BASELINE
-#             done
-#         done
-#     done
-# done
-
-
-for B in 50000
+Consider B sizes 10000, 30000, 50000
+Consider learning rates 0.005, 0.01, 0.02
+Use try different rtg  and nn_baseline settings
+for B in 10000 30000 50000
 do
-    for LR in 0.01 0.02
+    for LR in 0.005 0.01 0.02
     do
-        for RTG in "-rtg"
+        for RTG in "" "-rtg"
         do
-            for NN_BASELINE in "--nn_baseline"
+            for NN_BASELINE in "" "--nn_baseline"
             do
                 echo "Running with B=$B LR=$LR RTG=$RTG NN_BASELINE=$NN_BASELINE"
-                python rob831/scripts/run_hw2.py --env_name HalfCheetah-v4 --ep_len 150 --discount 0.95 -n 100 -l 2 -s 32 -b $B -lr $LR $RTG $NN_BASELINE --exp_name q7_b$B\_lr$LR$RTG$NN_BASELINE
+                python rob831/scripts/run_hw2.py --env_name HalfCheetah-v4 --ep_len 150 --discount 0.95 -n 100 -l 2 -s 32 -b $B -lr $LR $RTG $NN_BASELINE --exp_name q4_b$B\_lr$LR$RTG$NN_BASELINE
             done
         done
     done
